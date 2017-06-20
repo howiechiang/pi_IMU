ede5c4cb2eed400a879ae0053f2f69be45bac5c5
howiechiang <howiechiang@gmail.com>
2017-06-20 17:21:09 -0400

    Created a Common Library
    Moved common functions into a single library

--- IMU_I2C.py
+++ IMU_I2C.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
+from common import *
 import smbus
-import math
 import time
 import curses
+
 
 
 ########################################################################################################################
@@ -26,7 +27,7 @@ addr_z_accel = addr_accel + 4       # accel_zout address is 63
 # Sensor Scaling Values
 gyro_scale = 131.0
 accel_scale = 16384.0
-threshLastUpdatedReading = 5        # Signal error if the last reading was taken more than 5 seconds ago..
+threshLastUpdatedReading = 1       # Signal error if the last reading was taken more than 5 seconds ago..
 
 ########################################################################################################################
 # KALMAN'S FILTER SETTINGS
@@ -59,46 +60,46 @@ class IMU():
     # SENSOR COMMUNICATION [I2C]
     ####################################################################################################################
 
-    # Reads data from sensor's registers
-    def read_byte(self, adr):
-
-        try:
-           return self.bus.read_byte_data(self.address, adr)
-
-        except IOError as err:
-            print(err)
-
-    def read_word(self, adr):
-
-        try:
-            high = self.bus.read_byte_data(self.address, adr)
-            low = self.bus.read_byte_data(self.address, adr + 1)
-            val = (high << 8) + low
-            return val
-
-        except IOError as err:
-            print(err)
-
-    # Checks if the value is negative and performs 2's Complement if binary value is negative
-    def read_word_2c(self, adr):
-
-        val = self.read_word(adr)
-        if (val >= 0x8000):
-            return -((65535 - val) + 1)
-
-        else:
-            return val
-
-    # Write data to sensor's registers
-    def write_byte(self, adr, value):
-        try:
-            self.bus.write_byte_data(self.address, adr, value)
-
-        except IOError as err:
-            print(err)
+    # # Reads data from sensor's registers
+    # def read_byte(self, adr):
+    #
+    #     try:
+    #        return self.bus.read_byte_data(self.address, adr)
+    #
+    #     except IOError as err:
+    #         print(err)
+    #
+    # def read_word(self, adr):
+    #
+    #     try:
+    #         high = self.bus.read_byte_data(self.address, adr)
+    #         low = self.bus.read_byte_data(self.address, adr + 1)
+    #         val = (high << 8) + low
+    #         return val
+    #
+    #     except IOError as err:
+    #         print(err)
+    #
+    # # Checks if the value is negative and performs 2's Complement if binary value is negative
+    # def read_word_2c(self, adr):
+    #
+    #     val = self.read_word(adr)
+    #     if (val >= 0x8000):
+    #         return -((65535 - val) + 1)
+    #
+    #     else:
+    #         return val
+    #
+    # # Write data to sensor's registers
+    # def write_byte(self, adr, value):
+    #     try:
+    #         self.bus.write_byte_data(self.address, adr, value)
+    #
+    #     except IOError as err:
+    #         print(err)
 
     # Read all raw data from IMU & Compass
-    def readall_imu(self):
+    def get_Data(self):
 
         raw_gyro_data = self.bus.read_i2c_block_data(self.address, addr_gyro, 6)
         raw_accel_data = self.bus.read_i2c_block_data(self.address, addr_accel, 6)
@@ -115,7 +116,7 @@ class IMU():
 
     # Read all raw data and perform Kalman's filter
     # Accelerometer has noise, while the gyro has steady state drift
-    def readall_KalmanFilter(self):
+    def update_KalmanFilter(self):
 
         # This reduces the sampling rate of the data request
         time.sleep(time_diff - 0.005)
@@ -123,10 +124,11 @@ class IMU():
         # Update Time
         tNow = time.time()
         dt = tNow - self.tLastUpdated or 0
+        if dt > threshLastUpdatedReading: raise('The last updated reading was over a second ago....)')
         self.tLastUpdated = tNow
 
         gyro_xScaled, gyro_yScaled, gyro_zScaled, accel_xScaled, accel_yScaled, accel_zScaled \
-            = self.readall_imu()
+            = self.get_Data()
 
         # Subtract offset from raw gyro data. The offset value is determined during calibration
         gyro_xScaled -= self.gyro_xOffset
@@ -160,7 +162,7 @@ class IMU():
 
         # Read raw gyro & accel data from sensors
         self.gyro_xOffset, self.gyro_yOffset, self.gyro_zOffset, accel_xScaled, accel_yScaled, accel_zScaled \
-            = self.readall_imu()
+            = self.get_Data()
 
         # Convert accel data to rotation data
         self.xRotation = get_x_rotation(accel_xScaled, accel_yScaled, accel_zScaled)
@@ -168,35 +170,55 @@ class IMU():
         self.zRotation = get_z_rotation(accel_xScaled, accel_yScaled, accel_zScaled)
 
 
-########################################################################################################################
-# MATH / COMPUTATIONAL FUNCTIONS
-########################################################################################################################
+    # This utilizes curses library which clears refreshes the contents of the carriage on the command line
+    def display_Data(self):
 
-def twos_compliment(val):
-    if val >= 0x8000:
-        return-((65535 - val) + 1)
-    else:
-        return val
+        stdscr = curses.initscr()
 
-# Pythagorean theorem of two lengths
-def dist(a, b):
+        while True:
+            try:
 
-    return math.sqrt((a * a) + (b * b))
+                stdscr.clear()
 
-def get_y_rotation(x, y, z):
+                #### GYRO DATA ####
+                gyro_xout = read_word_2c(self.bus, self.address, addr_x_gyro)
+                gyro_yout = read_word_2c(self.bus, self.address, addr_y_gyro)
+                gyro_zout = read_word_2c(self.bus, self.address, addr_z_gyro)
 
-    radians = math.atan2(x, dist(y, z))
-    return -math.degrees(radians)
+                stdscr.addstr("Raw gyro data\n------------------\n")
+                stdscr.addstr("gyro_xout: " + str(gyro_xout) + " scaled: " + str(gyro_xout / gyro_scale) + "\n")
+                stdscr.addstr("gyro_yout: " + str(gyro_yout) + " scaled: " + str(gyro_yout / gyro_scale) + "\n")
+                stdscr.addstr("gyro_zout: " + str(gyro_zout) + " scaled: " + str(gyro_zout / gyro_scale) + "\n")
+                stdscr.addstr("\n")
 
-def get_x_rotation(x, y, z):
+                #### ACCELEROMETER DATA ####
+                accel_xout = read_word_2c(self.bus, self.address, addr_x_accel)
+                accel_yout = read_word_2c(self.bus, self.address, addr_y_accel)
+                accel_zout = read_word_2c(self.bus, self.address, addr_z_accel)
 
-    radians = math.atan2(y, dist(x, z))
-    return math.degrees(radians)
+                stdscr.addstr("Raw accelerometer data\n------------------\n")
+                stdscr.addstr("accel_xout: " + str(accel_xout) + " scaled: " + str(accel_xout / accel_scale) + "\n")
+                stdscr.addstr("accel_yout: " + str(accel_yout) + " scaled: " + str(accel_yout / accel_scale) + "\n")
+                stdscr.addstr("accel_zout: " + str(accel_zout) + " scaled: " + str(accel_zout / accel_scale) + "\n")
+                stdscr.addstr("\n")
 
-def get_z_rotation(x,y,z):
+                #### CALCULATED ROTATION DATA ####
+                stdscr.addstr("Rotation\n------------------\n")
+                stdscr.addstr("x rotation: " + str(get_x_rotation(accel_xout / accel_scale, accel_yout / accel_scale,
+                                                                  accel_zout / accel_scale)) + "\n")
+                stdscr.addstr("y rotation: " + str(get_y_rotation(accel_xout / accel_scale, accel_yout / accel_scale,
+                                                                  accel_zout / accel_scale)) + "\n")
+                stdscr.addstr("\n")
 
-    radians = math.atan2(z, dist(x,y))
-    return math.degrees(radians)
+                stdscr.refresh()
+                time.sleep(1)
+
+            except:
+
+                stdscr.keypad(0)
+                curses.echo()
+                curses.nocbreak()
+                curses.endwin()
 
 
 ########################################################################################################################
@@ -205,49 +227,8 @@ def get_z_rotation(x,y,z):
 if __name__ == "__main__":
 
     imu1 = IMU(addr_imu)
-    stdscr = curses.initscr()
+    imu1.display_Data()
 
     while True:
-        try:
+        imu1.update_KalmanFilter()
 
-            stdscr.clear()
-
-            #### GYRO DATA ####
-            gyro_xout = imu1.read_word_2c(addr_x_gyro)
-            gyro_yout = imu1.read_word_2c(addr_y_gyro)
-            gyro_zout = imu1.read_word_2c(addr_z_gyro)
-
-            stdscr.addstr("Raw gyro data\n------------------\n")
-            stdscr.addstr("gyro_xout: " + str(gyro_xout) + " scaled: " + str(gyro_xout / gyro_scale) + "\n")
-            stdscr.addstr("gyro_yout: " + str(gyro_yout) + " scaled: " + str(gyro_yout / gyro_scale) + "\n")
-            stdscr.addstr("gyro_zout: " + str(gyro_zout) + " scaled: " + str(gyro_zout / gyro_scale) + "\n")
-            stdscr.addstr("\n")
-
-            #### ACCELEROMETER DATA ####
-            accel_xout = imu1.read_word_2c(addr_x_accel)
-            accel_yout = imu1.read_word_2c(addr_y_accel)
-            accel_zout = imu1.read_word_2c(addr_z_accel)
-
-            stdscr.addstr("Raw accelerometer data\n------------------\n")
-            stdscr.addstr("accel_xout: " + str(accel_xout) + " scaled: " + str(accel_xout / accel_scale) + "\n")
-            stdscr.addstr("accel_yout: " + str(accel_yout) + " scaled: " + str(accel_yout / accel_scale) + "\n")
-            stdscr.addstr("accel_zout: " + str(accel_zout) + " scaled: " + str(accel_zout / accel_scale) + "\n")
-            stdscr.addstr("\n")
-
-            #### CALCULATED ROTATION DATA ####
-            stdscr.addstr("Rotation\n------------------\n")
-            stdscr.addstr("x rotation: " + str(get_x_rotation(accel_xout / accel_scale, accel_yout / accel_scale, accel_zout / accel_scale)) + "\n")
-            stdscr.addstr("y rotation: " + str(get_y_rotation(accel_xout / accel_scale, accel_yout / accel_scale, accel_zout / accel_scale)) + "\n")
-            stdscr.addstr("\n")
-
-            stdscr.refresh()
-            time.sleep(1)
-
-        except:
-
-            stdscr.keypad(0)
-            curses.echo()
-            curses.nocbreak()
-            curses.endwin()
-
-	
---
+++ common.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import math
+
+########################################################################################################################
+# MATH / COMPUTATIONAL FUNCTIONS
+########################################################################################################################
+
+def twos_compliment(val):
+    if val >= 0x8000:
+        return-((65535 - val) + 1)
+    else:
+        return val
+
+# Pythagorean theorem of two lengths
+def dist(a, b):
+
+    return math.sqrt((a * a) + (b * b))
+
+def get_y_rotation(x, y, z):
+
+    radians = math.atan2(x, dist(y, z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x, y, z):
+
+    radians = math.atan2(y, dist(x, z))
+    return math.degrees(radians)
+
+def get_z_rotation(x,y,z):
+
+    radians = math.atan2(z, dist(x,y))
+    return math.degrees(radians)
+
+def get_CompassRotation(x, y):
+
+    radians = math.atan2(y, x)
+    if radians < 0:
+        radians += 2 * math.pi
+    return math.degrees(radians)
+
+########################################################################################################################
+# SENSOR COMMUNICATION [I2C]
+########################################################################################################################
+
+# Reads data from sensor's registers
+def read_byte(bus, adr_device, adr_register):
+
+    try:
+       return bus.read_byte_data(adr_device, adr_register)
+
+    except IOError as err:
+        print(err)
+
+def read_word(bus, adr_device, adr_register):
+
+    try:
+        high = bus.read_byte_data(adr_device, adr_register)
+        low = bus.read_byte_data(adr_device, adr_register + 1)
+        val = (high << 8) + low
+        return val
+
+    except IOError as err:
+        print(err)
+
+# Checks if the value is negative and performs 2's Complement if binary value is negative
+def read_word_2c(bus, adr_device, adr_register):
+
+    val = read_word(bus, adr_device, adr_register)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+
+    else:
+        return val
+
+# Write data to sensor's registers
+def write_byte(bus, adr_device, adr_register, value):
+
+    try:
+        bus.write_byte_data(adr_device, adr_register, value)
+
+    except IOError as err:
+        print(err)
